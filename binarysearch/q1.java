716c210d3141b1c28e80442c7b006fbd1e2f508d
M Vishnu vardhan <vishnuvardhan7823@gmail.com>
2025-03-06 09:32:28 +0530

    binarysearch

---
+++ binarysearch/q1.java
@@ -0,0 +1,31 @@
+package binarysearch;
+
+public class q1 {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,5,6,8,9,10,12,34};
+        System.out.println(binarysearch(array, 10));
+        
+    }
+
+    public static int binarysearch(int[] arr, int target){
+
+        
+        int start = 0;
+        int end = arr.length-1;
+       while(start<=end){
+        int mid = start+(end-start)/2;
+
+        if(target<arr[mid]){
+            end = mid-1;
+        }
+        else if(target>arr[mid]){
+            start = mid+1;
+        }
+        else{
+            return mid;
+        }
+       }
+       return -1;
+
+    }
+}
